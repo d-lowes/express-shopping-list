7fd16250524658984446e2f0d6f3169957b16bb5
Jakob <shavinski.jakob@gmail.com>
2023-05-10 16:32:57 -0700

    finish get specific item route and patch route

--- fakeDb.js
+++ fakeDb.js
@@ -1,11 +1,13 @@
 "use strict";
 
+
+// FIXME: how can we use spaces with these in the URL? 
 const items = [
-  { name: "Curried Duck", price: 14.50 },
-  { name: "Dark Souls", price: 69.69 },
+  { name: "CurriedDuck", price: 14.50 },
+  { name: "DarkSouls", price: 69.69 },
   { name: "PS5", price: 420.69 },
-  { name: "Trinidadian Rum", price: 17.40 },
-  { name: "Nicki Minaj CD", price: 11.99 }
+  { name: "TrinidadianRum", price: 17.40 },
+  { name: "NickiMinajCD", price: 11.99 }
 ];
 
 module.exports = { items };
--- itemRoutes.js
+++ itemRoutes.js
@@ -3,21 +3,63 @@
 const express = require('express')
 
 const db = require("./fakeDb");
-
+const { NotFoundError, BadRequestError } = require("./expressError")
 const router = new express.Router();
 
 /** GET /items: return list list of shopping items: */
-router.get("/", function (req, res) {
+router.get("/", function (req, res, next) {
+
   return res.json(db.items);
 });
 
 /** POST /items: accept JSON body, add item, and return it: */
-router.post("/", function (req, res) {
-  const itemToAdd = {name: "popsicle", price: 1.45};
+router.post("/", function (req, res, next) {
+  //FIXME: should we just use insomnia for post or do it through web page?
+  const itemToAdd = req.body;
 
   db.items.push(itemToAdd);
 
-  return res.json({added: itemToAdd});
+  return res.json({ added: itemToAdd });
 });
+
+/** GET /items/:name: return single item: {name: popsicle, "price": 1.45}*/
+router.get("/:name", function (req, res, next) {
+
+  const itemName = req.params.name;
+
+
+  // TODO: make a helper function in separate file
+  for (let item of db.items) {
+    if (itemName === item.name) {
+      return res.json({ item });
+    }
+  }
+
+  throw new NotFoundError;
+});
+
+router.patch("/:name", function (req, res, next) {
+  const itemName = req.params.name;
+  const updateItem = req.body;
+  console.log(updateItem);
+
+  for (let item of db.items) {
+    if (itemName === item.name) {
+      item.name = updateItem.name;
+      item.price = updateItem.price;
+
+      return res.json({ updated: item });
+    }
+  }
+});
+
+router.delete("/:name", function (req, res, next) {
+
+  
+});
+
+
+
+
 
 module.exports = router;
